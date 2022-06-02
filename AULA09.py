75776ad34b998be034584c7b5b56752895ad7b2f
Eduardo Teixeira <103132551+eduteixeirabhz@users.noreply.github.com>
2022-06-02 13:39:11 -0300

    Add files via upload

---
+++ AULA09.py
@@ -0,0 +1,18 @@
+frase = "Curso em Vídeo Python"
+print(frase[0:14])
+# Imprime na tela os  caracteres selecionados
+print(frase.count('o'))
+# Conta quantidade de letras
+print(frase.upper().count('o'))
+# Conta quantidade de letras em maiusculo
+print(len(frase))
+#contagem de caracteres
+print(frase.replace('Python', 'Android'))
+#Substitui palavras
+#Toda String é IMUTÁVEL em seus microelementos a não ser que se faça uma atribuição
+print(frase.find('Curso'))
+#Mostra em qual posição está a palavra selecionada
+print(frase.upper())
+print(frase.lower())
+print(frase.split())
+print(frase.join())
